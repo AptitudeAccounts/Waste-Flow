54aa908beeb9ab3fe1a2ab0da6df73ca16afcb13
Aptitude Group <accounts@aptitude.ae>
2026-07-02 14:49:38 +0400

    Update config.js

--- web/assets/config.js
+++ web/assets/config.js
@@ -6,7 +6,7 @@
  *   https://script.google.com/macros/s/AKfycb.../exec
  */
 const WASTEFLOW_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzE5VDOXVTbOcBOCBCSy7KXTiJ6lmLb5tA1RwLZL1AYD2eU4_SkY3jZhXRFnTU_dN76xQ/exec',
+  API_URL: https://script.googleusercontent.com/a/macros/aptitude.ae/echo?user_content_key=AUkAhnQ8HIbpZZ_agvHXeN2MTAUNpfJdYxIMqRq7QYiSJuoAuVookwVcy4iyYwyejwmBKjV5Ab2yZ5ufl1z1r1M57L2K9R0jOcFQp9Q6ySRJenClGMGL1diotiyXikktzz6rjZadMJCbM15Mo1oMeHikDh9FLHFJC_XMEX6V2e8lulFGm8Hd3mELkPfi6BiOGbcwPrMrmldKOrW8v6BqV2oEd_4Ej0nSYE2cPeGL6CNv98zG0wD4ClfDju0qYIpdLrpjSnMvzWFXAugXnF3znl_22f6h8ECa_dU5-x2j2nuhKXs0KcDpRc0&lib=MvqlEKV_U9AvamOXf8FhF7kXOc5imR5D5,
   SHEET_URL: 'https://docs.google.com/spreadsheets/d/1DHSast77Nz7KcjLztIrqQJOwkXYZN2V4NpxgNxucxV0/edit',
   COMPANY_NAME: 'Your F&B Group'
 };
