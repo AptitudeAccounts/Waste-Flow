051254c84df550d90ccd08e10407991c016cbc4c
Aptitude Group <accounts@aptitude.ae>
2026-07-02 14:38:16 +0400

    Update config.js

--- web/assets/config.js
+++ web/assets/config.js
@@ -6,7 +6,7 @@
  *   https://script.google.com/macros/s/AKfycb.../exec
  */
 const WASTEFLOW_CONFIG = {
-  API_URL: https://script.google.com/macros/s/AKfycbzE5VDOXVTbOcBOCBCSy7KXTiJ6lmLb5tA1RwLZLlAYD2eU4_SkY3jZhXRFnTU_dN76xQ/exec,
-  SHEET_URL:https://docs.google.com/spreadsheets/d/1DHSast77Nz7KcjLztIrqQJOwkXYZN2V4NpxgNxucxV0/edit,
+  API_URL: 'https://script.google.com/macros/s/AKfycbzE5VDOXVTbOcBOCBCSy7KXTiJ6lmLb5tA1RwLZL1AYD2eU4_SkY3jZhXRFnTU_dN76xQ/exec',
+  SHEET_URL: 'https://docs.google.com/spreadsheets/d/1DHSast77Nz7KcjLztIrqQJOwkXYZN2V4NpxgNxucxV0/edit',
   COMPANY_NAME: 'Your F&B Group'
 };
